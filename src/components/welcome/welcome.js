0a9d8cc7e293ebbb62e656a92bfaf2f84684191f
Carlos Costa <carllos.nc@gmail.com>
2019-09-04 14:51:30 -0300

    some fixes

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -3,7 +3,7 @@ import React from "react"
 const Welcome = () => (
   <section>
     <h1> React init pack. </h1>
-    <p>A simple way to start a react package to npm.</p>
+    <p>A simple way to start a react package for npm.</p>
     <p>
       Access
       <a href="https://github.com/C4co/react-init-pack"> Github </a>
